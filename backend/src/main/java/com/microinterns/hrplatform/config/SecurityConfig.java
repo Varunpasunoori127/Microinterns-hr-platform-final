ccbf464e5de694df1991def23e2a1b60159536d1
Varun Pasunoori <varunpasunoori2002@gmail.com>
2026-04-09 23:01:59 +0100

    fix cors for vercel

--- backend/src/main/java/com/microinterns/hrplatform/config/SecurityConfig.java
+++ backend/src/main/java/com/microinterns/hrplatform/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "http://127.0.0.1:5173"
+                "https://microinterns-hr-platform-final.vercel.app"
         ));
 
         config.setAllowedMethods(List.of(
